60cb6ea390b06831719e62bb965df1ea273d5795
Alena_Statsenko <alena7worlds@gmail.com>
2021-12-16 14:57:04 +0300

    cards menu added

--- js/main.js
+++ js/main.js
@@ -136,4 +136,49 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     window.addEventListener('scroll', showModalByScroll);
+
+    //Menu with cards
+    class MenuCard {
+        constructor(img, alt, title, descr, price, container) {
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27;
+            this.container = document.querySelector(container);
+            this.changeToUAH();
+        };
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        };
+
+        render() {
+            const card = document.createElement('div');
+            card.classList.add('menu__item');
+            card.innerHTML = `
+            <img src=${this.img} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>`;
+            this.container.append(card);
+        };
+    };
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд',
+        8,
+        '.menu__field .container'
+    ).render();
 });
+
+
+
